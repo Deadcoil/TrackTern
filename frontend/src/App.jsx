3edbd91e81694982a56178f5e198f612cffc0ba2
Prakash Behuria <beingstudentloving009@gmail.com>
2026-02-18 13:28:01 +0530

    Initial Project Setup for Frontend and Backend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className="h-screen flex items-center justify-center bg-blue-500">
+      <h1 className="text-4xl text-white font-bold">
+        Intern Management System 🚀
+      </h1>
+    </div>
+  )
+}
+
+export default App;
